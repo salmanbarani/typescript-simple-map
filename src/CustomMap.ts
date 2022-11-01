e4ceaf970597e50cdc35ba4db2df17e77cd7afe6
salmanbarani <salman.barani@postpay.io>
2022-11-01 23:03:14 +0300

    setup and finish project

---
+++ src/CustomMap.ts
@@ -0,0 +1,42 @@
+
+
+export interface Mappable {
+    location :  {
+        lat: number;
+        lng: number;
+    };
+    markerContent(): string; 
+}
+
+
+export class CustomMap {
+    private googleMap : google.maps.Map
+
+    constructor(id: string) {
+        this.googleMap = new google.maps.Map(document.getElementById(id), {
+            zoom: 1,
+            center: {
+                lat: 0,
+                lng: 0
+            }
+        });
+    }
+
+    addMarker(mappable: Mappable) {
+        const marker = new google.maps.Marker({
+                map: this.googleMap,
+                position: {
+                    lat: mappable.location.lat,
+                    lng: mappable.location.lng
+                }
+            });
+
+        marker.addListener('click', () => {
+            const infoWindow = new google.maps.InfoWindow({
+                content: mappable.markerContent()
+            });
+
+            infoWindow.open(this.googleMap, marker);
+        })
+    }
+}
